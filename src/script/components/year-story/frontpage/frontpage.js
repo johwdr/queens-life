d955198943dc1c476a012d7ca3d5e699a9a06df7
Johannes Wehner <johw@dr.dk>
2020-03-30 20:44:40 +0200

    NEw frontpage.

--- src/script/components/year-story/frontpage/frontpage.js
+++ src/script/components/year-story/frontpage/frontpage.js
@@ -17,13 +17,22 @@ export default class Frontpage {
 
         this.frontpageContent = document.createElement('div');
 
-        this.frontpageContent.innerHTML = `
+        /*this.frontpageContent.innerHTML = `
 
             <h1><span class="story-text-hilite">Hvad lavede dronningen, da hun var på din alder</span></h1>
+        `;*/
+        this.frontpageContent.innerHTML = `
+
+            <h1>Hvad lavede dronningen, da hun var på din alder</h1>
         `;
         this.container.appendChild(this.frontpageContent)
 
         this.container.appendChild(this.selector)
+
+        const helpTextElement = document.createElement('div');
+        helpTextElement.classList.add('help-text');
+        helpTextElement.innerText = 'Indtast dit fødselsår'
+        this.container.appendChild(helpTextElement)
 
         return {
             container: this.container
