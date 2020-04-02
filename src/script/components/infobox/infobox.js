69ff3cc238aaa1fab06fc34420a5bb34971d24c1
Johannes Wehner <johw@dr.dk>
2020-04-02 14:56:00 +0200

    Design adjuesmtnes

--- src/script/components/infobox/infobox.js
+++ src/script/components/infobox/infobox.js
@@ -3,23 +3,23 @@
 import './infobox.scss';
 
 const closerSVG = `
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    <svg version="1.1" id="closer" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 70 70" style="enable-background:new 0 0 70 70;" xml:space="preserve">
         <g>
-            <rect x="0" y="33.6" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -14.4975 35)" class="st0" width="70" height="2.8"/>
-            <rect x="33.6" y="0" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -14.4975 35)" class="st0" width="2.8" height="70"/>
+            <rect x="0" y="33.6" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -14.4975 35)" class="info-element" width="70" height="2.8"/>
+            <rect x="33.6" y="0" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -14.4975 35)" class="info-element" width="2.8" height="70"/>
         </g>
     </svg>
 `
 
 const infoSVG = `
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    <svg version="1.1" id="opener" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 70 70" style="enable-background:new 0 0 70 70;" xml:space="preserve">
         <g>
-            <path class="st0" d="M35,70C15.7,70,0,54.3,0,35S15.7,0,35,0s35,15.7,35,35S54.3,70,35,70z M35,3.6C17.7,3.6,3.6,17.7,3.6,35
+            <path class="info-element" d="M35,70C15.7,70,0,54.3,0,35S15.7,0,35,0s35,15.7,35,35S54.3,70,35,70z M35,3.6C17.7,3.6,3.6,17.7,3.6,35
                 S17.7,66.4,35,66.4S66.4,52.3,66.4,35S52.3,3.6,35,3.6z"/>
             <g>
-                <path class="st0" d="M35,21.7c-1.9,0-3.2-1.3-3.2-3c0-1.7,1.3-3,3.2-3c1.8,0,3.1,1.3,3.1,3C38.1,20.4,36.8,21.7,35,21.7z
+                <path class="info-element" d="M35,21.7c-1.9,0-3.2-1.3-3.2-3c0-1.7,1.3-3,3.2-3c1.8,0,3.1,1.3,3.1,3C38.1,20.4,36.8,21.7,35,21.7z
                         M37.4,50.3h-4.8V26.5h4.8V50.3z"/>
             </g>
         </g>
--- src/script/components/year-story/end/end.js
+++ src/script/components/year-story/end/end.js
@@ -40,7 +40,7 @@ export default class End {
         this.container.classList.add('end-slide-hidden');
 
         this.endSlideContent = document.createElement('div');
-
+        this.endSlideContent.classList.add('next-text')
         this.container.appendChild(this.timer.wrapper)
 
         this.yearSelectButton = document.createElement('button');
@@ -64,7 +64,17 @@ export default class End {
 
         this.link = document.createElement('a');
         this.link.id = 'end-more-link'
-        this.link.innerText = this.config['linktekst']
+        this.link.innerHTML = this.config['linktekst'] + `
+        <svg version="1.1" class="end-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox = "0 0 12.2 7.05" style = "enable-background:new 0 0 12.2 7.05;" xml: space = "preserve" >
+                    <style type="text/css">
+                        .st0{fill:#496C60;}
+                    </style>
+                    <g>
+                        <path class="st0" d="M8.61,1.03c0,0.69,0,1.27,0,1.91c-2.49,0-4.9,0-7.32,0c0,0.5,0,0.93,0,1.43c2.46,0,4.87,0,7.37,0
+                        c0,0.62,0,1.17,0,1.66c0.86-0.86,1.7-1.69,2.44-2.42C10.35,2.82,9.53,1.98,8.61,1.03z"/>
+                    </g>
+        </svg>`;
         this.link.href = this.config['linkurl']
         console.log(this.config)
 
@@ -90,7 +100,16 @@ export default class End {
             this.currentLinkIndex = 0;
         }
 
-        this.yearSelectButton.innerText = 'Vælg et andet år';
+        this.yearSelectButton.innerHTML = `Vælg et andet år <svg version="1.1" class="end-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox = "0 0 12.2 7.05" style="enable-background:new 0 0 12.2 7.05;" xml: space = "preserve" >
+                    <style type="text/css">
+                        .st0{fill:#496C60;}
+                    </style>
+                    <g>
+                        <path class="st0" d="M8.61,1.03c0,0.69,0,1.27,0,1.91c-2.49,0-4.9,0-7.32,0c0,0.5,0,0.93,0,1.43c2.46,0,4.87,0,7.37,0
+                        c0,0.62,0,1.17,0,1.66c0.86-0.86,1.7-1.69,2.44-2.42C10.35,2.82,9.53,1.98,8.61,1.03z"/>
+                    </g>
+        </svg>`;
         this.nextYear = next.aarstal;
 
         this.endSlideContent.innerHTML = `Går til året ${next.aarstal}, ${next.text}`;
